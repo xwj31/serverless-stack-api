7dfb4dcccfa2cdddc93d6a74d23de88b5500af7f
xwj31 <>
2020-01-27 16:51:54 +0000

    adding api

--- delete.js
+++ delete.js
@@ -1,23 +1,21 @@
-import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
-    Item: {
+    // 'Key' defines the partition key and sort key of the item to be removed
+    // - 'userId': Identity Pool identity id of the authenticated user
+    // - 'noteId': path parameter
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      noteId: uuid.v1(),
-      content: data.content,
-      attachment: data.attachment,
-      createdAt: Date.now()
+      noteId: event.pathParameters.id
     }
   };
 
   try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
+    await dynamoDbLib.call("delete", params);
+    return success({ status: true });
   } catch (e) {
     return failure({ status: false });
   }
--- libs/response-lib.js
+++ libs/response-lib.js
@@ -1,11 +1,9 @@
 export function success(body) {
     return buildResponse(200, body);
   }
-  
   export function failure(body) {
     return buildResponse(500, body);
   }
-  
   function buildResponse(statusCode, body) {
     return {
       statusCode: statusCode,
